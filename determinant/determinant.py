3710478fa79aac356f492289727259e79a929b0d
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2022-08-10 18:55:41 +0500

    added tests for the determinant function

---
+++ determinant/determinant.py
@@ -0,0 +1,8 @@
+def determinant(matrix: [[int]]) -> int:
+    """Calculates the value of the matrix determinant
+
+    :param matrix: an integer matrix
+    :return: the value of the matrix determinant
+    """
+
+    pass
