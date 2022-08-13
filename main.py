c7db3c7c6b19bb1531a3afc7ab1dac1fd9855650
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2022-08-13 23:14:38 +0500

    Update permutation function and tests

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def get_permutations(digit_cnt: int) -> list[str]:
+def generate_permutations(digit_cnt: int, result_output: list[str],
+                          prefix: list[int] = None, level: int = -1) -> None:
+    """Generates permutations of digits and adds them into the result_output
+    list.
+
+    :param digit_cnt: number of digits used for permutations.
+    :param result_output: a list for adding permutation strings.
+    :param prefix: a list of generated digits.
+    :param level: generation level.
+    :raise Exception: when the digit_cnt value is None.
+    :return: None
+    """
     pass
 
 
--- test_permutations.py
+++ test_permutations.py
@@ -1,31 +1,41 @@
 import unittest
 
-from main import get_permutations
+from main import generate_permutations
 
 
 class TestPermutation(unittest.TestCase):
     def test_none(self):
-        self.assertRaises(Exception, get_permutations, None)
+        self.assertRaises(Exception, generate_permutations, None, [])
 
     def test_0(self):
-        self.assertCountEqual(get_permutations(0), [])
+        result = []
+        generate_permutations(0, result)
+        self.assertCountEqual(result, [])
 
     def test_1(self):
-        self.assertCountEqual(get_permutations(1), ['1'])
+        result = []
+        generate_permutations(1, result)
+        self.assertCountEqual(result, ['1'])
 
     def test_2(self):
-        self.assertCountEqual(get_permutations(2), ['12', '21'])
+        result = []
+        generate_permutations(2, result)
+        self.assertCountEqual(result, ['12', '21'])
 
     def test_3(self):
-        result = ['123', '132', '213', '231', '312', '321']
-        self.assertCountEqual(get_permutations(3), result)
+        result = []
+        generate_permutations(3, result)
+        self.assertCountEqual(result,
+                              ['123', '132', '213', '231', '312', '321'])
 
     def test_4(self):
-        result = ['1234', '1243', '1324', '1342', '1423', '1432', '2134',
-                  '2143', '2314', '2341', '2413', '2431', '3124', '3142',
-                  '3214', '3241', '3412', '3421', '4123', '4132', '4213',
-                  '4231', '4312', '4321']
-        self.assertCountEqual(get_permutations(4), result)
+        result = []
+        generate_permutations(4, result)
+        self.assertCountEqual(result, ['1234', '1243', '1324', '1342', '1423',
+                                       '1432', '2134', '2143', '2314', '2341',
+                                       '2413', '2431', '3124', '3142', '3214',
+                                       '3241', '3412', '3421', '4123', '4132',
+                                       '4213', '4231', '4312', '4321'])
 
 
 if __name__ == '__main__':
