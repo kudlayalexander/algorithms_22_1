b7a4c1730eb153ee9293f5d1593260c82fb7aad9
Ilya <tyulin600@gmail.com>
2022-09-10 10:51:56 +0500

    git commit -m ‘Реализованы рекурсивная и итеративная функции вычисления чисел Фибоначчи, а также функция поиска определителя матрицы’

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import time
 from typing import Any
 
@@ -5,21 +6,30 @@ from typing import Any
 def fibonacci_rec(num: int) -> int:
     """Returns the fibonacci number according to the number specified in the
     parameter. Recursive implementation.
-
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    if num == 0:
+        return 1
+    if num == 1:
+        return 0
+    return fibonacci_rec(num - 2) + fibonacci_rec(num - 1)
 
 
 def fibonacci_iter(num: int) -> int:
     """Returns the fibonacci number according to the number specified in the
     parameter. Iterative implementation.
-
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    if num == 1:
+        return 0
+    if num == 2 or num == 3:
+        return 1
+    fib1 = fib2 = 1
+    for i in range(3, num):
+        fib1, fib2 = fib2, fib1 + fib2
+    return fib2
 
 
 def determinant(matrix: [[int]]) -> int:
@@ -28,7 +38,41 @@ def determinant(matrix: [[int]]) -> int:
     :raise Exception: when the parameter value is not a square matrix
     :return: the value of the matrix determinant
     """
-    pass
+    l = len(matrix)
+    for i in range(l):
+        if len(matrix[i]) != l:
+            raise Exception("Матрица не квадратная")
+    if l == 1:
+        return matrix[0][0]
+    if l == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    # Определяем строку с наибольшим кол-вом нулей
+    most_nulls_count = 0
+    most_nulls_row = 0
+    current_row = 0
+    for row in matrix:
+        null_count = 0
+        for number in row:
+            if number == 0:
+                null_count += 1
+        if null_count > most_nulls_count:
+            most_nulls_row = current_row
+        current_row += 1
+    sum = 0
+    for i in range(l):
+        matrix_copy = copy.deepcopy(matrix)
+        for j in range(l):
+            for k in range(l):
+                matrix_copy[most_nulls_row][k] = None
+                matrix_copy[j][i] = None
+        for j in range(l):
+            matrix_copy[j] = [x for x in matrix_copy[j] if x is not None]
+        new_matrix = []
+        for j in range(l):
+            if len(matrix_copy[j]) != 0:
+                new_matrix.append(matrix_copy[j])
+        sum += matrix[most_nulls_row][i] * determinant(new_matrix) * ((-1) ** i)
+    return sum
 
 
 def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
