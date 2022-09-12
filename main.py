7a49264e1a915e78eae40fcd576b95fecc31eac8
rootersupport <tomlenov16@gmail.com>
2022-09-12 16:27:17 +0500

    ВСЁ РЕАЛИЗОВАНО

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
 def gcd_recursive(a: int, b: int) -> int:
     """Calculates the greatest common divisor of two numbers.
     Recursive implementation.
-
     :param a: first number
     :param b: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception("a or b is None")
+    if a == b:
+        return a
+    if a * b == 0:
+        return a + b
+    if b > a:
+        a, b = b, a
+    return gcd_recursive(a - b, b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
     """Calculates the greatest common divisor of two numbers.
     Iterative implementation using subtraction.
-
     :param a: first number
     :param b: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception("a or b is None")
+    while a != b and a * b != 0:
+        if a < b:
+            a, b = b, a
+        a -= b
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
     """Calculates the greatest common divisor of two numbers
     Iterative implementation using division.
-
     :param a: first number
     :param b: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception("a or b is None")
+    while b:
+        a, b = b, a % b
+    return a
 
 
 def lcm(a: int, b: int) -> int:
     """Calculates the least common multiple of two numbers
-
     :param a: first number
     :param b: second number
     :except Exception: when a or b value is None
     :return: the least common multiple
     """
-    pass
+    if a is None or b is None:
+        raise Exception("a or b is None")
+    return int(a * b / gcd_iterative_fast(a, b))
 
 
 def main():
